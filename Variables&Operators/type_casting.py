9e2c7a1f5a45219179deeea3221cb9c9a3788031
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-25 22:36:10 +0530

    practice python variables and operators

---
+++ Variables&Operators/type_casting.py
@@ -0,0 +1,13 @@
+marks = 99.92
+print(marks, "\nType of number ", marks,  " is: " , type(marks))\
+  
+# Casting type of float variable into int
+marks = int(marks)
+print(type(marks))
+
+str = "231.44"
+print(str, "\nType of number ", str,  " is: " , type(str))
+
+decimalNum = float(str)
+print(decimalNum)
+print(type(decimalNum))
