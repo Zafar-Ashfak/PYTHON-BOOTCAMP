6dff3647a4ce195d21cab52444ee674f983ccda6
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-25 21:56:13 +0530

    practice python variables and operators

---
+++ Variables&Operators/operators.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 4
+
+# Operators
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 % num2)
+
---
+++ Variables&Operators/var.py
@@ -0,0 +1,15 @@
+a = 10 # a is an integer variable
+print(a , "-> Type: ", type(a))
+
+b = 15.5 # b is a float variable
+print(b , "-> Type: ", type(b))
+
+name = "Md Ashfaq Alam" # name is a string variable
+print(name , "-> Type: ", type(name))
+
+isActive = True # isActive is a boolean variable
+print(isActive , "-> Type: ", type(isActive))
+
+num = None # num is a empty variable
+print(num , "-> Type: ", type(num))
+
