ceacf3e795a4054e24d163a01b11d8fd119d2f1b
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-25 16:19:25 +0530

    practice python fundamentals

---
+++ Practice-Questions/myIntro.py
@@ -0,0 +1,11 @@
+intro = """
+Hello, I'm Md Ashfak Alam.
+
+Welcome to my profile!!!
+
+
+I am a results-driven software developer with a strong foundation in building scalable, high-performance applications. I specialize in Java backend development using Core Java, Spring, Spring Boot, RESTful APIs, JDBC, Hibernate/JPA, and MySQL.
+I am also bring solid frontend experience with React.js, Redux Toolkit, Context API, HTML5, and CSS3.With strong proficiency in Data Structures & Algorithms, Java, C, and C++, I focus on writing clean, efficient, and maintainable code. I am actively seeking opportunities as a Java Backend Developer or Full Stack Developer where I can contribute to impactful projects and continue growing as an engineer.
+"""
+
+print(intro)
