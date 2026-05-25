ad660992abb87737ac0e1c5b7b567d64c5aec0cc
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-25 16:47:26 +0530

    practice python fundamentals

---
+++ Practice-Questions/os_module.py
@@ -0,0 +1,12 @@
+import os
+
+# Select the directory path
+path = "/"
+
+# Get the list of files and folders
+contents = os.listdir(path)
+
+print("Contents of the directory:")
+
+for item in contents:
+    print(item)
