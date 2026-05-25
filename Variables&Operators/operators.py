d447b69997d7729176e88a22637b6aee7eb59255
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-25 22:14:29 +0530

    practice python variables and operators

--- Variables&Operators/operators.py
+++ Variables&Operators/operators.py
@@ -1,7 +1,8 @@
 num1 = 10
 num2 = 4
 
-# Operators
+# Arithmetic Operators
+print("ARITHMETIC OPERATORS")
 print(num1 + num2)
 print(num1 - num2)
 print(num1 * num2)
@@ -9,3 +10,45 @@ print(num1 / num2)
 print(num1 ** num2)
 print(num1 % num2)
 
+
+# Assignment Operators
+print("ASSIGNMENT OPERATORS")
+
+a = 10
+a += 5
+print(a)
+
+b = 20
+b -= 10
+print(b)
+
+c = 45
+c *= 10
+print(c)
+
+d = 60
+d /= 6
+print(d)
+
+#Comparison Operators -> return true or false
+print("COMPARISON OPERATORS")
+print(10 == 10) # True
+print(10 == 15) # False
+print(10 > 5) # True
+print(10 > 15) # False
+print(10 >= 5) # True
+print(10 >= 15) # False
+print(2 < 5) # True
+print(12 < 5) # False
+print(11 <= 55) # True
+print(10 <= 5) # False
+print(10 != 3) # True
+print(10 != 10) # False
+
+# Logical Operators
+print("LOGICAL OPERATORS")
+print(10 >= 15 or 15 <= 5) # False
+print(10 <= 15 or 15 >= 5) # True
+print(10 <= 15 and 15 >= 5) #True
+print(10 <= 15 and 15 <= 5) #False
+print(not(True)) # False
