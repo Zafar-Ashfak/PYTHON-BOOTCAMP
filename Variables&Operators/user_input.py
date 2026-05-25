98cd61ee66e21b51ab3f8d33769c2fdce70f15e4
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-25 22:46:00 +0530

    practice python variables and operators

---
+++ Variables&Operators/user_input.py
@@ -0,0 +1,10 @@
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))  
+
+# print(num1, " + ", num2, " = " , (num1 + num2))
+
+# Concatination of two Strings by user's input
+str1 = input("Enter your first name: ")
+str2 = input("Enter your last name: ")
+
+print("Your full name is:", str1 , "" + str2)
