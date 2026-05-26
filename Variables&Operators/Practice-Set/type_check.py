c413cb30aeca1bc3551b0a30a05d44acee353d7b
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-26 15:48:09 +0530

    practice python variables and operators

--- Variables&Operators/Practice-Set/type_check.py
+++ Variables&Operators/Practice-Set/type_check.py
@@ -1,9 +1,18 @@
-num = input("Enter a number: ")
+num1 = input("Enter an integer number: ")
+num2 = input("Enter an float number: ")
 
-check = type(num)
+check1 = type(num1)
 
-print("Before type casting input type is: ", check)
+print("Before type casting input type is: ", check1)
 
-num = int(num)
-check = type(num)
-print("After type casting input type is: ", check)
+num1 = int(num1)
+check1 = type(num1)
+print("After type casting input type is: ", check1)
+
+print("\n")
+
+check2 = type(num2)
+print("Before type casting input type is: ", check2)
+num2 = float(num2)
+check2 = type(num2)
+print("After type casting input type is: ", check2)
