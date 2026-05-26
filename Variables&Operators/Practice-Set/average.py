6517266f5cf2c8b4f7b89943676faf7f50791b4a
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-26 16:05:14 +0530

    practice python variables and operators

---
+++ Variables&Operators/Practice-Set/average.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number: "))
+
+num2 = int(input("Enter the second number: "))
+
+avg = (num1 + num2) / 2
+
+print("Average of ", num1, "and", num2,"is", avg)
---
+++ Variables&Operators/Practice-Set/square.py
@@ -0,0 +1,9 @@
+# Program to calculate the square of a number
+
+num = int(input("Enter an integer number: "))
+
+square = 1
+
+square = num ** 2
+
+print(square)
