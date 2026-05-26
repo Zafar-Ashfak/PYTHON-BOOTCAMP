880b228b5bd9abed37acdb4b78851f4ddfbe01d7
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-26 11:51:47 +0530

    practice python variables and operators

---
+++ Variables&Operators/Practice-Set/remainder.py
@@ -0,0 +1,9 @@
+# Write a program to get remainder when a number is divided by z
+
+num1 = int(input("Enter the dividend: "))
+
+num2 = int(input("Enter the divisor: "))
+
+remiander = num1 % num2
+
+print(num1, "%", num2, "=", remiander)
