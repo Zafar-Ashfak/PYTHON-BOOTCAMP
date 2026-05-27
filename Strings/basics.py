474c57d50d5218185e73886553c0ffe32dc05096
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-27 21:37:05 +0530

    practice python variables and operators

---
+++ Strings/basics.py
@@ -0,0 +1,18 @@
+from builtins import print
+
+firstName = 'Zafar'
+
+lastName = "Ashfaq"
+
+str = """
+Hello I am Md Ashfaq Alam.
+I am a software engineer with 1 year of experience.
+"""
+
+print(firstName, "" + lastName)
+print(str)
+
+#String Slicing
+
+shortStr = str[0 : 15]
+print(shortStr)
