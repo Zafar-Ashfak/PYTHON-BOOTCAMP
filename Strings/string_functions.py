7b23cc937dfb5ce8f902abb4b2b7fca59fc79353
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-29 22:15:32 +0530

    practice python variables and operators

---
+++ Strings/string_functions.py
@@ -0,0 +1,16 @@
+name = "zafar ashfaq"
+
+print("Length of the string is:", len(name))
+
+print(name.startswith("Za")) # True
+
+print(name.endswith("aq")) # True
+
+print(name.capitalize()) # Capitalize the first char of the string
+
+print(name.lower())
+
+print(name.upper())
+
+print(name.title())
+
