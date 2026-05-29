56660a3ecd0436d0cb8be9b7f683c3c9048cff5d
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-29 22:22:21 +0530

    practice python variables and operators

--- Strings/string_functions.py
+++ Strings/string_functions.py
@@ -1,4 +1,4 @@
-name = "zafar ashfaq"
+name = "   hello i am a senior software  engineer    "
 
 print("Length of the string is:", len(name))
 
@@ -8,9 +8,16 @@ print(name.endswith("aq")) # True
 
 print(name.capitalize()) # Capitalize the first char of the string
 
-print(name.lower())
+print(name.lower()) # Converts to lowercase
 
-print(name.upper())
+print(name.upper()) # Converts to uppercase
 
-print(name.title())
+print(name.title())  # Capitalizes each word
 
+print("Letter 'a' occurs", name.count("a"), "times in the given string")
+
+print(name.strip()) # Removes spaces from both ends
+
+print(name.lstrip()) # Removes spaces from left side
+
+print(name.rstrip()) # Removes spaces from right side
