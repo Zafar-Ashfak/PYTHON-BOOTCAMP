e0f57166c3f34f7229f3d75988ee4450a523300d
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-30 23:03:39 +0530

    practice list & tuple programs

---
+++ Lists&Tuples/Practice-Set/Q1.py
@@ -0,0 +1,24 @@
+fruits = []
+
+f1 = input("Enter 1st fruit name: ")
+fruits.append(f1)
+
+f2 = input("Enter 2nd fruit name: ")
+fruits.append(f2)
+
+f3 = input("Enter 3rd fruit name: ")
+fruits.append(f3)
+
+f4 = input("Enter  4th fruit name: ")
+fruits.append(f4)
+
+f5 = input("Enter 5th fruit name: ")
+fruits.append(f5)
+
+f6 = input("Enter 6th fruit name: ")
+fruits.append(f6)
+
+f7 = input("Enter 7th fruit name: ")
+fruits.append(f7)
+
+print(fruits)
