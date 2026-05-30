b2bb2125ab2ca2727c91cf4cf85dfca034006480
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-05-30 23:13:12 +0530

    practice list & tuple programs

---
+++ Lists&Tuples/Practice-Set/Q2.py
@@ -0,0 +1,19 @@
+marksOfStudents = []
+
+s1 = int(input("Enter your marks: "))
+marksOfStudents.append(s1)
+
+s2 = int(input("Enter your marks: "))
+marksOfStudents.append(s2)
+
+s3 = int(input("Enter your marks: "))
+marksOfStudents.append(s3)
+s4 = int(input("Enter your marks: "))
+marksOfStudents.append(s4)
+s5 = int(input("Enter your marks: "))
+marksOfStudents.append(s5)
+s6 = int(input("Enter your marks: "))
+marksOfStudents.append(s6)
+
+print(f"Marks of students {marksOfStudents}")
+print("After sorting the marks of the students\n", sorted(marksOfStudents))
