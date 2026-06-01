b2ada60f982a56ce7ecd815c2f734046be994e29
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-01 18:12:08 +0530

    practice set functions

--- Set/sets.py
+++ Set/sets.py
@@ -1,10 +1,24 @@
 # nums = {} # dict
 
 # We can store different types of data in the set
-nums = {1, 2, 5, 8, "Zafar", 4.6, True, None} # set
+mySet = {1, 2, 5, 8, "Zafar", 4.6, True, None} # set
 
+print(mySet)
+print(type(mySet))
+
+uniqueVal = {1 , 3, 5, 7, 3, 6, 8, 5 }
+print(uniqueVal) # Output -> {1, 3, 5, 6, 7, 8} prints unique val only
+
+# <<<<<<<<<<<<<<<<<< SET FUNCTIONS >>>>>>>>>>>>>>>>>>
+
+nums = {1, 2, 3, 4, 5}
 print(nums)
-print(type(nums))
 
+# 1. add() method -> adds a single element at the end
+nums.add(6)
+print(nums)
 
+# 2. update() method -> Adds multiple elements from another iterable.
+nums.update({7, 8, 9, 10})
+print(nums)
 
