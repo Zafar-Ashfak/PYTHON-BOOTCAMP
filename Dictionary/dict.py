4c02a5ccda2d767ac03a74d185e0aa96066a3f1d
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-01 12:43:27 +0530

    practice dictionary programs

---
+++ Dictionary/dict.py
@@ -0,0 +1,18 @@
+employee_details = {
+    "id": 102340,
+    "name" : "Zafar Ashfak",
+    "email" : "Zafarashfak008@gmail.com",
+    "address": "Arvind Nagar Colony, Tolichowki, Hyderabad"
+
+}
+
+print(employee_details)
+print(type(employee_details))
+
+print(f"Employee Id: {employee_details["id"]}")
+print(f"Name: {employee_details["name"]}")
+print(f"Email: {employee_details["email"]}")
+print(f"Address: {employee_details["address"]}")
+
+
+
