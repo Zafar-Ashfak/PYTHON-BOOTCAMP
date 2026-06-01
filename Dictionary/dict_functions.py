8bccd655f43dd7c417284acb87160b2b3375358e
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-01 13:48:49 +0530

    practice dictionary functions

--- Dictionary/dict_functions.py
+++ Dictionary/dict_functions.py
@@ -12,15 +12,15 @@ print(student, "\n")
 # <<<<<<<<<<<<<<<<< DICTIONARY FUNCTIONS >>>>>>>>>>>>>>>>>>>>
 
 # 1. get() method
-print(f"Student Id: {student.get("id")}")
-print(f"Name: {student.get("name")}")
-print(f"Roll: {student.get("roll")}")
-print(f"Age: {student.get("age")} years")
-print(f"Marks: {student.get("marks")} CGPA")
-print(f"Address: {student.get("address")}\n")
+print(f"Student Id: {student.get('id')}")
+print(f"Name: {student.get('name')}")
+print(f"Roll: {student.get('roll')}")
+print(f"Age: {student.get('age')} years")
+print(f"Marks: {student.get('marks')} CGPA")
+print(f"Address: {student.get('address')}\n")
 
 # get() method with default value
-print(f"Contact : {student.get("contact", "Not Found")}\n")
+print(f"Contact : {student.get('contact', 'Not Found')}\n")
 
 # 2. keys method -> return all keys of the dictionary
 print(student.keys())
@@ -33,12 +33,14 @@ print(student.items())
 
 # 5. update() method -> Updates existing keys or adds new ones
 print(f"\nAfter updating marks and contact of {student.get('name')}")
-student.update({"marks": "9.5 CGPA"})
-student.update({"contact": "7480820409"})
+student.update({
+    "marks": 9.5,
+    "contact": 7480820409
+})
 print(student)
 
 # 6. pop() method -> Removes a key and returns its value
-print(f"\nStudent Age: {student.pop("age")}\n")
+print(f"\nStudent Age: {student.pop('age')}\n")
 
 # 7. popitem() method -> Removes and returns the last inserted key-value pair
 print(student.popitem(), "\n")
@@ -50,3 +52,9 @@ print("New student details by mistake deleted from database\n", new_student)
 # 9. setdefault() method -> Returns the value of a key. If the key doesn't exist, inserts it with a default value.
 student.setdefault("age", 10)
 print("\n", student)
+
+# 10. clear() method -> clears the dictionary data
+# print(student.clear())
+
+# 11. len() method
+print(f"\nLength of the student dictionary is: {len(student)}")
