c397cfc8a06c26723c668ed15f26483af2565bce
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-02 17:34:17 +0530

    practice set functions

---
+++ Dictionary/Practice-Set/fav_lang.py
@@ -0,0 +1,20 @@
+my_dict = {}
+
+friend1 = input("Enter 1st friend name: ")
+fav_lang1 = input("Enter your favorite language: ")
+
+friend2 = input("Enter 2nd friend name: ")
+fav_lang2 = input("Enter your favorite language: ")
+
+friend3 = input("Enter 3rd friend name: ")
+fav_lang3 = input("Enter your favorite language: ")
+
+friend4 = input("Enter 4th friend name: ")
+fav_lang4 = input("Enter your favorite language: ")
+
+my_dict[friend1] = fav_lang1
+my_dict[friend2] = fav_lang2
+my_dict[friend3] = fav_lang3
+my_dict[friend4] = fav_lang4
+
+print(my_dict)
