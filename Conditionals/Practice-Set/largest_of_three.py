9801056123594b0db0efd58e84d11397bc3f7173
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-03 20:50:58 +0530

    practice conditional questions program

--- Conditionals/Practice-Set/largest_of_three.py
+++ Conditionals/Practice-Set/largest_of_three.py
@@ -3,9 +3,9 @@ num2 = int(input("Enter the second number: "))
 num3 = int(input("Enter the third number: "))
 if num1 == num2 == num3:
     print("All numbers are equal")
-elif num1 >= num2 and num1 >= num3:
+elif num1 > num2 and num1 > num3:
     print(f"{num1} is the largest number among: {num1}, {num2} and {num3}")
-elif num2 >= num3:
+elif num2 > num3:
     print(f"{num2} is the largest number among: {num1}, {num2} and {num3}")
 else:
     print(f"{num3} is the largest number among: {num1}, {num2} and {num3}")
