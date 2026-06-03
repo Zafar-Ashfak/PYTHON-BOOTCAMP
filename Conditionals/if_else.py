ce804fac431bdb0092e407b7861a7fc443ecc908
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-03 13:52:07 +0530

    practice conditional questions program

--- Conditionals/if_else.py
+++ Conditionals/if_else.py
@@ -1,6 +1,6 @@
 age = int(input("Enter your age: "))
 
 if age >= 18:
-    print("You are eligible to elect the leader")
+    print("You are eligible to vote")
 else:
-    print("Your are not eligible to elect the leader")
+    print("Your are not eligible vote")
