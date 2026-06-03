8762cd7721db540859ad2c2d936b3186231b181c
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-03 20:05:25 +0530

    practice conditional questions program

---
+++ Conditionals/Practice-Set/max.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+
+max_num = max(num1, num2, num3, num4)
+print(f"Largest number is: {max_num}")
