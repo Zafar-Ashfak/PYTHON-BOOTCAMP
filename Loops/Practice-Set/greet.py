3c75e1980e2eaaa8280f707bbe4ff5ff6506eefa
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-04 13:29:05 +0530

    practice loops program

---
+++ Loops/Practice-Set/greet.py
@@ -0,0 +1,5 @@
+l = ["Ashfaq", "Rahbar", "Irfan", "Ruhul", "Aftab", "Isha", "Mobashshir", "Anwar", "Mukhtar", "Fazil", "Anas"]
+
+for e in l:
+    if e.startswith('R') or e.startswith('A'):
+        print(f"Good Morning: {e}")
