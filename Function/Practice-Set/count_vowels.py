3f7657d6b71bf0e0469d7920a23974397d486f3e
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-05 22:40:16 +0530

    practice function programs

--- Function/Practice-Set/count_vowels.py
+++ Function/Practice-Set/count_vowels.py
@@ -1,1 +1,14 @@
 # Write a function count_vowels(text) that returns the number of vowels in a string.
+def count_vowels(text):
+    count = 0
+    text = text.lower()
+    for i in text:
+        if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+            count += 1
+
+    return count
+
+
+user_input = input("Enter a text: ")
+res = count_vowels(user_input)
+print(f"Total number of vowels in the text is: {res}")
