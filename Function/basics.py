678f2655de25943dff08c9511e3a79695f3ba1a0
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-05 19:56:26 +0530

    practice function programs

---
+++ Function/basics.py
@@ -0,0 +1,7 @@
+def avg():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    num3 = int(input("Enter the third number: "))
+    print((num1 + num2 + num3) / 3)
+avg()
+
---
+++ Function/take_nothing_return_nothing.py
@@ -0,0 +1,6 @@
+def product():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(f"{num1} X {num2} = {num1 * num2}")
+
+product()
