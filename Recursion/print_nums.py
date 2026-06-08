8543923cd8cbba124f035d40ca910a29956c6f9b
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-08 18:30:53 +0530

    practice recursion programs

---
+++ Recursion/print_nums.py
@@ -0,0 +1,14 @@
+# Program to print numbers from 1 to n
+def nums(n):
+    #Base Case
+    if n <= 0:
+        return
+    nums(n - 1)
+    print(n, end=" ")
+
+def main():
+    print("Program to print numbers from 1 to n")
+    n = int(input("Enter a natural number: "))
+    nums(n)
+
+main()
