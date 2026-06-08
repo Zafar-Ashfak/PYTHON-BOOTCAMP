9286ec6bedcae8aba238ec6b4185329f652559db
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-08 18:50:08 +0530

    practice recursion programs

--- Recursion/sum_of_n_nums.py
+++ Recursion/sum_of_n_nums.py
@@ -1,1 +1,16 @@
 # Write a program to calculate the sum of first n natural numbers
+def sum_of_nums(n):
+    s = 0
+    # Base Case
+    if n == 0:
+        return 0
+
+    # Work
+    return n + sum_of_nums(n - 1)
+
+def main():
+    print("Program to calculate the sum of first n natural numbers")
+    n = int(input("Enter a number: "))
+    res = sum_of_nums(n)
+    print(f"Sum of first {n} natural numbers is: {res}")
+main()
