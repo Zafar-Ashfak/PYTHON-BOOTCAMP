70ef18cc9560cea39c7a7927190ec6d915058efd
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-10 20:52:58 +0530

    practice recursion programs

---
+++ Function/Practice-Set/sum_of_all_elements.py
@@ -0,0 +1,17 @@
+# Write a program to calculate the sum of all the elements in the list
+def elements_sum(nums, n):
+    s = 0
+    # Base Case
+    if n == 0:
+        return nums[0]
+
+    # Recursive Call
+    return nums[n] + elements_sum(nums, n - 1)
+
+def main():
+    print("Program to calculate the sum of all the elements in the list")
+    nums = [1, 3, 2, 4, 10, 7, 4]
+    res = elements_sum(nums, len(nums) - 1)
+    print(f"Sum of all the elements in the list is: {res}")
+
+main()
