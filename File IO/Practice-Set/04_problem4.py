f71afe7837b1f2c677b6c5a0063f9e8c0161ef66
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-11 22:48:54 +0530

    practice File I/O programs

---
+++ File IO/Practice-Set/04_problem4.py
@@ -0,0 +1,14 @@
+# A file contains a word "Donkey" multiple times. You need to write a program which replace this word with "Horse" by updating the same file.
+
+with open("donkey_story.txt", "r") as file:
+    data = file.read()
+
+    new_data = data.replace("Donkey", "Horse")
+
+with open("donkey_story.txt", "w") as update_file:
+    update_file.write(new_data)
+
+print("File updated successfully\n")
+
+with open("donkey_story.txt", "r") as read_file:
+    print(read_file.read())
