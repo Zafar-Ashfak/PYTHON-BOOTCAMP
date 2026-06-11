f2101813fa412441d3b6949c62ff31679619c07e
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-11 21:03:50 +0530

    practice File I/O programs

---
+++ File IO/Practice-Set/01_problem1.py
@@ -0,0 +1,11 @@
+# Write a program to read user's name from a given file 'names.txt' and find whether it contains or not.
+
+name = input("Enter your full name: ").lower()
+
+with open("names.txt") as file:
+    data = file.read().lower().splitlines()
+
+    if name in data:
+        print("Name found")
+    else:
+        print("Name not found")
