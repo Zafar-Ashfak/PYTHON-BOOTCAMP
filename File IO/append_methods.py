01bcef44231c16a0ec64792f3104435172978ce1
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-12 22:46:48 +0530

    practice File I/O programs

--- File IO/append_methods.py
+++ File IO/append_methods.py
@@ -1,6 +1,7 @@
-message = input("Enter a message: ")
+# Write a program that asks the user for some text and appends it to a file.
+text = input("Enter some texts: ")
 
 # append method
 file = open("myfile.txt", "a")
-file.write(f"{message}\n")
+file.write(f"{text}\n")
 file.close()
