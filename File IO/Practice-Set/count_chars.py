bf89712b1a45e2b4b3736cf6bdf5409d8f14a00f
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-12 20:52:41 +0530

    practice File I/O programs

---
+++ File IO/Practice-Set/count_chars.py
@@ -0,0 +1,9 @@
+# Write a program to count the total number of characters in file
+with open("file.txt", "r") as file:
+    data = file.read()
+    count = 0
+    for char in data:
+        count += 1
+
+    print(f"Total number of characters in the file 'file.txt' is: {count}")
+
