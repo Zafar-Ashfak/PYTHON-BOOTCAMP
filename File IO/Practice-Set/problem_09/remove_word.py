7b7ebecdecb2c1869f14f60b08d10db4d2b3223b
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-13 15:05:19 +0530

    practice File I/O programs

---
+++ File IO/Practice-Set/problem_09/remove_word.py
@@ -0,0 +1,21 @@
+# Write a program that removes all occurrences of a given word from a file.
+
+def remove_word(filename, matching_word):
+    with open(filename, "r") as file:
+        data = file.read().lower()
+        new_data = data.replace(matching_word, "")
+
+    with open(filename, "a") as file:
+        file.write(f"\n\nAfter Removing all occurrences of word '{matching_word}'\n\n")
+        file.write(new_data)
+        print("Removed all occurrences of a given word successfully")
+
+
+def main():
+    print("Program that removes all occurrences of a given word from a file")
+    filename = "file.txt"
+    word = "rabbit"
+    remove_word(filename, word)
+
+main()
+
