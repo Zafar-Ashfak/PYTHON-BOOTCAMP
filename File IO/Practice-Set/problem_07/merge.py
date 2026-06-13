fa9d64aa74ee0739bdc32b7719befce279d275de
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-13 14:09:26 +0530

    practice File I/O programs

---
+++ File IO/Practice-Set/problem_07/merge.py
@@ -0,0 +1,25 @@
+# Write a program to merge the contents of two files into a third file.
+
+def merge(file1, file2, file3):
+    with open(file1, "r") as f1:
+        data1 = f1.read()
+
+    with open(file2, "r") as f2:
+        data2 = f2.read()
+
+
+    with open(file3, "w") as f3:
+        f3.write(data1)
+        f3.write("\n\n")
+        f3.write(data2)
+        print("Contents of two files have merged successfully")
+
+def main():
+    print("Program to merge the contents of two files into a third file")
+    file1 = "file1.txt"
+    file2 = "file2.txt"
+    file3 = "file3.txt"
+
+    merge(file1, file2, file3)
+
+main()
