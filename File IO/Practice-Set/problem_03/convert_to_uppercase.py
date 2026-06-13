c6cb9e68428685dec26f8a31ff643e02da0393e8
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-13 12:13:53 +0530

    practice File I/O programs

---
+++ File IO/Practice-Set/problem_03/convert_to_uppercase.py
@@ -0,0 +1,8 @@
+# Write a program that reads a file and change its contents into uppercase and append in the same file.
+
+with open("aladdin.txt", "r") as file:
+    data = file.read()
+
+with open("aladdin.txt", "a") as uppercase:
+    uppercase.write(f"\n\n{data.upper()}")
+    print("Converted to uppercase and appended successfully")
