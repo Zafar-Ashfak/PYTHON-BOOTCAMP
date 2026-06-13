06bb50644eaee85ee369ad9ef9d7d3ad77cb1be0
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-13 12:51:07 +0530

    practice File I/O programs

---
+++ File IO/Practice-Set/problem_05/remove_blank_lines.py
@@ -0,0 +1,12 @@
+# Write a program to create a new file that contains
+# all non-empty lines from another file.
+
+with open("crow.txt", "r") as source:
+    lines = source.readlines()
+
+with open("newfile.txt", "w") as new_file:
+    for line in lines:
+        if line.strip():  # Check if the line is not empty
+            new_file.write(line)
+
+print("Non-empty lines copied successfully.")
