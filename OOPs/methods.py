2ec32495357e676b17dc54adbf3614333cf49ce1
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-14 20:07:20 +0530

    practice oops concepts

---
+++ OOPs/methods.py
@@ -0,0 +1,22 @@
+class Car:
+    brand = "Pagani" # class attribute
+    wheel = 4 # class attribute
+    def __init__(self, color, engine):
+        self.color = color # obj attribute
+        self.engine = engine # obj attribute
+        print("Car is created...")
+
+    def change_color(self, color):
+        self.color = color
+        return color
+
+
+c1 = Car("Red", "V8")
+print(f"Brand: {c1.brand}")
+print(f"Color: {c1.color}")
+print(f"Engine: {c1.engine}")
+
+print(f"New color after changing: {c1.change_color("Blue")}")
+
+
+
