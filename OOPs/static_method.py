6d3914b2a28aa81566faf45a3e19b4bec3a196f4
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-15 18:35:09 +0530

    practice oops concepts

---
+++ OOPs/static_method.py
@@ -0,0 +1,14 @@
+class Employee:
+    company = "Google"
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def greet():
+        print(f"Welcome to {Employee.company}", end= " ")
+
+
+e1 = Employee("Zafar Ashfak")
+Employee.greet() # Calling static method
+print(e1.name)
