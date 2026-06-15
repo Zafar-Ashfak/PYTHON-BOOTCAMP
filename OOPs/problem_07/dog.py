834790fc9ee3fa9fd047fa60eb4267757472caa6
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-15 13:28:11 +0530

    practice oops concepts

---
+++ OOPs/problem_07/dog.py
@@ -0,0 +1,33 @@
+# Create a Dog class with:
+#
+# Class attribute: species = "Dog"
+# Object attributes: name, age
+# Methods:
+# bark()
+# display_info()
+
+class Dog:
+    species = "Dog"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("Woof woof!")
+
+    def display_info(self):
+        print(f"Dog name: {self.name}, age: {self.age} years")
+
+
+# Creating objects of class Dog
+d1 = Dog("Siberian Huskey", 13)
+print(f"Species: {d1.species}")
+d1.display_info()
+d1.bark()
+
+print()
+
+d2 = Dog("BullDog", 8)
+print(f"Species: {d2.species}")
+d2.display_info()
+d2.bark()
