61f5a136290763ae2e481f791b94b84f5bc36362
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-16 20:34:36 +0530

    practice dictionary programs

--- Set/practice_set/displayUniqueNums.py
+++ Set/practice_set/displayUniqueNums.py
@@ -1,28 +1,28 @@
 # Program to take input from the user and display all the unique numbers (once)
-numsSet = set()
+num_set = set()
 
 num = int(input("Enter the 1st number: "))
-numsSet.add(num)
+num_set.add(num)
 
 num = int(input("Enter the 2nd number: "))
-numsSet.add(num)
+num_set.add(num)
 
 num = int(input("Enter the 3rd number: "))
-numsSet.add(num)
+num_set.add(num)
 
 num = int(input("Enter the 4th number: "))
-numsSet.add(num)
+num_set.add(num)
 
 num = int(input("Enter the 5th number: "))
-numsSet.add(num)
+num_set.add(num)
 
 num = int(input("Enter the 6th number: "))
-numsSet.add(num)
+num_set.add(num)
 
 num = int(input("Enter the 7th number: "))
-numsSet.add(num)
+num_set.add(num)
 
 num = int(input("Enter the 8th number: "))
-numsSet.add(num)
+num_set.add(num)
 
-print(f"Display only unique values: {numsSet}")
+print(f"Display only unique values: {num_set}")
--- Set/practice_set/getLength.py
+++ Set/practice_set/getLength.py
@@ -1,4 +1,4 @@
-# Q (4). What will be the length of the set?
+# Q (4). What will be the length of the set s?
 s = set()
 s.add(20)
 s.add(20.0)
@@ -6,6 +6,6 @@ s.add('20')
 
 print(f"Length of the set is: {len(s)}") # Output -> 2
 
-# Q(5). What is the type?
+# Q(5). What is the type object a?
 a = {}
 print(type(a)) # Output -> <class 'dict'>
