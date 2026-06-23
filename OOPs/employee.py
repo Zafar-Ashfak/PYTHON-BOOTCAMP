25c992b94473b3bd0b614c2b58953b4688cc61c8
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-23 23:08:06 +0530

    practice oops programs

--- OOPs/employee.py
+++ OOPs/employee.py
@@ -1,23 +1,32 @@
 class Employee:
-    company = "Google"
+    company = "Amazon"
 
-    def __init__(self, name, role, salary):
-        self.name = name
+    def __init__(self, role, department, salary):
         self.role = role
+        self.department = department
         self.salary = salary
 
-    def display_info(self):
-        print(f"Employee name: {self.name}")
+    def show_details(self):
         print(f"Role: {self.role}")
         print(f"Salary: {self.salary} LPA")
         print(f"Company: {self.company}")
 
 
+class Engineer(Employee):
+    def __init__(self, name, age, role, department, salary):
+        super().__init__(role, department, salary)
+        self.name = name
+        self.age = age
+
+    def show_details(self):
+        print(f"Employee Name: {self.name}")
+        print(f"Age: {self.age}")
+        super().show_details()
+        print()
+
 # Creating Student objects
-e1 = Employee("Md Ashfak Alam", "Java Developer", 2700000)
-e1.display_info()
+e1 = Engineer("Saba Zafar", 25,"Transportation Specialist", "Supply Chain", 450000)
+e1.show_details()
 
-print()
-
-e2 = Employee("Rahbar Hussain", "Python Full Stack Developer", 3400000)
-e2.display_info()
+e2 = Engineer("Zafar Ashfaq", 27, "Java Backend Developer", "Software Development", 3400000)
+e2.show_details()
