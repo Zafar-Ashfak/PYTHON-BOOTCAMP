559aca1e237e34e747495754a463ed7e3dfe086d
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-06-23 18:20:51 +0530

    practice inheritance programs

--- OOPs/property_keyword/property.py
+++ OOPs/property_keyword/property.py
@@ -1,23 +1,28 @@
 class Student:
     college = "MANUU"
 
-    def __init__(self, name, marks):
+    def __init__(self, name, phy, che, math):
         self.name = name
-        self.marks = marks
+        self.phy = phy
+        self.che = che
+        self.math = math
+        # self.percentage = f"{(self.phy + self.che + self.math) / 3:.2f} %"
 
-    def get_percentage(self):
-        s = 0
-        for e in self.marks:
-            s += e
-
-        print(f"Total percentage: {s / 3}%")
+    @property
+    def percentage(self):
+        return f"{(self.phy + self.che + self.math) / 3:.2f} %"
 
 
 def main():
-    s1 = Student("Saba Anjum", [99, 94, 97])
-    s1.get_percentage()
+    s1 = Student("Saba Anjum", 99, 94, 97)
+    print(f"Student Name: {s1.name}")
+    print(f"College: {s1.college}")
+    print(f"Marks: {s1.percentage}")
 
-    s1.marks[1] = 95
-    s1.get_percentage()
+    print("After changing marks in physics")
+    s1.math = 98
+    print(f"Final Marks: {s1.percentage}")
+
+
 
 main()
