a5d5fde5f19d3ac69d3f8148d4dcfb49469e41a3
Md_Ashfak_Alam <Zafarashfak008@gmail.com>
2026-07-10 20:39:51 +0530

    Print greetings for names starting with R or A

--- Loops/practice_set/greet.py
+++ Loops/practice_set/greet.py
@@ -1,3 +1,6 @@
+# Write a Python program that iterates through a list of names and prints a greeting only for
+# those names that start with the letters 'R' or 'A'.
+
 l = ["Rahbar", "Irfan", "Ashfaq", "Saba", "Zoya" ,"Ruhul", "Aftab", "Isha", "Anwar", "Mukhtar", "Fazil", ""]
 
 for name in l:
